cec91c0a4b3240808722ca7667ddedeef0005abc
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-03-22 17:20:08 +0200

    sol

--- src/api_helpers.ts
+++ src/api_helpers.ts
@@ -1,4 +1,4 @@
-export const API_NEWS = 'http://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=54d7035315ac4ea9a1a10cc43d923af5';
+export const API_NEWS = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=54d7035315ac4ea9a1a10cc43d923af5';
 
 export const getData = async (url: string) => {
   const response = await fetch(url);
